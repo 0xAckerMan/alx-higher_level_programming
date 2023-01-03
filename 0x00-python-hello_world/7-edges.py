9e6a2ad6a09af1313972c6ef3c6255aff41b4823
0xAckerMan <joelkores1@gmail.com>
2023-01-03 23:56:08 +0300

    Task 7: More slicing

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_last_2, word_first_3, middle_word = word[-2:],word[:3],word[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")

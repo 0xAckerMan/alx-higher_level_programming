2ec4fe233d16437e16e519b71229b71292edbed7
0xAckerMan <joelkores1@gmail.com>
2023-01-06 13:57:07 +0300

    Task 2: prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
 
-if len(argv) == 1:
-    print('0 arguments.')
-elif len(argv) == 2:
-    print('1 argument:')
-    print(f'{len(argv)-1:d}: {argv[len(argv)-1]:d}')
-else:
-    print(f"{len(argv)-1:d} arguments:")
-    for i in range(1, len(argv)):
-        print(f'{i:d}: {argv[i]:s}')
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print(f"{1:d}: {argv[1]:s}")
+    else:
+        print(f"{len(argv)-1} arguments:")
+        for i in range(1, len(argv)):
+            print(f"{i:d}: {argv[i]:s}")

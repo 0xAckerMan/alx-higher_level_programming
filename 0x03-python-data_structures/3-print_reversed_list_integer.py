36d3cf467f8c6a2bf6359f9198bc269139d3a81e
0xAckerMan <joelkores1@gmail.com>
2023-01-09 14:48:36 +0300

    Task 3: Reverse of a int list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
 def print_reversed_list_integer(my_list=[]):
-    for i in my_list:
-        print ('{}\n'.format(my_list[-i]), end='')
+    if not my_list:
+        return
+
+    for item in range(1, len(my_list) + 1):
+        print('{:d}'.format(my_list[-item]))

c76b80da5aa4d4537c4afe6ce12cf5a2e0770d91
0xAckerMan <joelkores1@gmail.com>
2023-01-10 17:01:10 +0300

    Task 5: Remove a char in a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    list_string = list(my_string)
+    '''list_string = list(my_string)
     for i in list_string:
         if i == 'c' or i == 'C':
             a = list_string.remove(i)
     list_string = ''.join([str(i) for i in list_string])
     return list_string
+    '''
+
+
+def no_c(my_string):
+    return "".join([i for i in my_string if i not in "cC"])

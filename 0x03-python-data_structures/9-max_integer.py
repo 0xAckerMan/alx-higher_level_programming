d6d6cc532f76a338fe5f0e21eb968a9f5701e97a
0xAckerMan <joelkores1@gmail.com>
2023-01-22 16:58:04 +0300

    Task 9: Searches for the biggest num

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        big_no = my_list[0]
+        for number in my_list:
+            if number > big_no:
+                big_no = number
+        return big_no
+
+
+if __name__ == "__main__":
+    my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+    max_value = max_integer(my_list)
+    print("Max: {}".format(max_value))

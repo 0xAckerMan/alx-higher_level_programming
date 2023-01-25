d906c141e53bff406151a34a4b76ea5e651eb21c
0xAckerMan <joelkores1@gmail.com>
2023-01-25 23:20:23 +0300

    Task 4: Setter and property

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -20,7 +20,7 @@ class Square:
                 raise TypeError('size must be an integer')
             elif size < 0:
                 raise ValueError('size must be >= 0')
-            size = self.__size
+            self.__size = size
 
         def area(self):
             return (self.__size ** 2)

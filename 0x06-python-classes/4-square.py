8aefdc516e64d738dd5b02e4445961f0efc52c61
0xAckerMan <joelkores1@gmail.com>
2023-01-25 23:16:42 +0300

    Task 4: Setter and property

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+Defines a class sqaure checks its attribute with
+area func
+'''
+
+
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+        @property
+        def size(self):
+            return self.__size
+
+        @size.setter
+        def size(self):
+            if type(size) != int:
+                raise TypeError('size must be an integer')
+            elif size < 0:
+                raise ValueError('size must be >= 0')
+            size = self.__size
+
+        def area(self):
+            return (self.__size ** 2)

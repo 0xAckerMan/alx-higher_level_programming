6c912d06691848510e9d92659e2c670a6104566f
0xAckerMan <joelkores1@gmail.com>
2023-01-26 00:45:35 +0300

    Task 5: Printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -2,17 +2,17 @@
 '''
 Implementation of the square class
 '''
+
+
 class Square:
     ''' Class implementation'''
     def __init__(self, size=0):
         self.size = size
 
-    
     @property
     def size(self):
         return self.__size
 
-    
     @size.setter
     def size(self, size):
         if type(size) != int:
@@ -23,12 +23,12 @@ class Square:
 
     def area(self):
         '''Calc square area'''
-        return(self.__size ** 2)
+        return (self.__size ** 2)
 
     def my_print(self):
         '''Prints a square'''
         if (self.__size == 0):
             print('')
-        
+
         for _ in range(self.__size):
             print("#" * self.__size)

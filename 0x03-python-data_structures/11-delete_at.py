1b87ad5ca5a92d340ee6d74ddc652fc4314bb1cf
0xAckerMan <joelkores1@gmail.com>
2023-01-27 18:09:31 +0300

    Task 11: Deletes at index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,8 @@
 
 def delete_at(my_list=[], idx=0):
     if idx < 0:
-        print(my_list)
+        return(my_list)
+
     for i in range(len(my_list)):
         if i == my_list[idx]:
             my_list.remove(i)

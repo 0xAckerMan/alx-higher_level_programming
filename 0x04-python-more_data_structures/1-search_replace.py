b7a45fecdb4b9df46ac30440551604d567b4f0b5
0xAckerMan <joelkores1@gmail.com>
2023-01-29 18:35:14 +0300

    Task 1: Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,8 +4,10 @@ def search_replace(my_list, search, replace):
     '''
     Works well too
     return [replace if x == search else x for x in my_list]'''
+    index = 0
     new_list = my_list.copy()
     for x in new_list:
         if x == search:
-            new_list[x] = replace
+            new_list[index] = replace
+        index =+ 1
     return new_list

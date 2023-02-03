0925135a3adea93eeb172276903f6a0cedecd96c
0xAckerMan <joelkores1@gmail.com>
2023-02-03 13:47:50 +0300

    Task 0: Adds two integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+'''
+This func add two integers
+'''
+
 
 def add_integer(a, b=98):
+    '''
+    function that returns sum(a, b)
+    '''
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):

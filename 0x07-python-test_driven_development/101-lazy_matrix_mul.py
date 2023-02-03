3fd1f43c6c9303efdffa1a5ed5c2406ab37ff5e8
0xAckerMan <joelkores1@gmail.com>
2023-02-04 02:42:53 +0300

    Advanced

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Defines lazy_matrix function
+"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ calculates the matrix multiplication of two matrices"""
+    return numpy.matmul(m_a, m_b)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+
+    Terms
+    Privacy
+    Security
+    Status
+    Docs
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+

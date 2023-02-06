58ecdd61207e231b074a90403acf547576ee5fad
0xAckerMan <joelkores1@gmail.com>
2023-02-06 21:05:34 +0300

    Task 4: Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module implements  a function that returns True if the object
+is an instance of a class that inherited (directly or indirectly)
+from the specified class ; otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    '''Function implementation'''
+
+    if isinstance(obj, a_class) and type(obj) != a_class:
+        return True
+    return False

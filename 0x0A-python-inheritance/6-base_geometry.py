8fa1858a347ec5f7d588457a5e65d7007c2f7cbb
0xAckerMan <joelkores1@gmail.com>
2023-02-06 23:02:15 +0300

    Task 6: Improove Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+Class BaseGeometry
+'''
+
+
+class BaseGeometry():
+    '''Class implementation'''
+
+    def area(self):
+        raise Exception("area() is not implemented")

1ad152cb4edffbc1a9fea132d0ac906f034b721c
0xAckerMan <joelkores1@gmail.com>
2023-02-07 23:57:58 +0300

    Advanced task 2

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -14,6 +14,10 @@ if __name__ == '__main__':
     counter = 0
 
     def print_stats(stats: dict, file_size: int) -> None:
+        '''
+        func implementation
+        '''
+
         print("File size: {:d}".format(file_size))
         for k, v in sorted(stats.items()):
             if v:

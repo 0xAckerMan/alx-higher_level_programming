bb5d7512c71c4629670ad86a843280fa1dc777c5
0xAckerMan <joelkores1@gmail.com>
2023-02-07 22:31:39 +0300

    Task 4: From json to object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+From json to object
+'''
+import json
+
+
+def from_json_string(my_str):
+    '''
+    func implementation
+    uses the loads object
+    '''
+
+    return json.loads(my_str)

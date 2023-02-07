c2eb4b66c90b5653d9114b28c29a893238af7b6d
0xAckerMan <joelkores1@gmail.com>
2023-02-07 20:26:38 +0300

    Task 0: prints file content

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,7 @@ Reads a utf8 file and prints it in stdout
 
 
 def read_file(filename=""):
+    ''' the function'''
+
     with open(filename, encoding="utf-8") as file:
         print(file.read(), end='')

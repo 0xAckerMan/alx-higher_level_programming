4da3d8f2c904ec310298c557adb9507de348df35
0xAckerMan <joelkores1@gmail.com>
2023-02-12 13:57:41 +0300

    Task 1. Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python3
 '''
-
+class base
 '''
+
+
+class Base:
+    ''' class implementation'''
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''
+        Class constructor
+        ARGS:
+            id which is setted to None
+        '''
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

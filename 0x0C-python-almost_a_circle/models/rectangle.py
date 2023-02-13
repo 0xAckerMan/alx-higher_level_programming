84a895eb57b23a742e299eeea708b434dc949ec9
0xAckerMan <joelkores1@gmail.com>
2023-02-13 11:31:32 +0300

    Chore: Implement getter method for all attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,3 +26,70 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        '''Width getter'''
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''width setter'''
+
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        elif value <= 0:
+            raise ValueError('width must be > 0')
+
+        self.__width = value
+
+    @property
+    def height(self):
+        '''Height  getter'''
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''Height attribute setter method'''
+
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value <= 0:
+            raise ValueError('height must be > 0')
+
+        self.__height = value
+
+    @property
+    def x(self):
+        '''X attribute getter method'''
+
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        '''Setter method for x attribute'''
+
+        if not isinstance(value, int):
+            raise TypeError('x must be an integer')
+        elif value < 0:
+            raise ValueError('x must be >= 0')
+
+        self.__x = value
+
+    @property
+    def y(self):
+        '''getter method for the y attribute'''
+
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        '''Setter method for the y attribute'''
+
+        if not isinstance(value, int):
+            raise TypeError('y must be an integer')
+        elif value < 0:
+            raise ValueError('y must be >= 0')
+        self.__y = value

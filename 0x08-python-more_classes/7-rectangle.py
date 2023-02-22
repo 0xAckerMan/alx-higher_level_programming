4ea7ad5838ef3b84a66060b10bdf322bec04ac76
0xAckerMan <joelkores1@gmail.com>
2023-02-22 22:59:54 +0300

    Created the width setter

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -15,4 +15,12 @@ class Rectangle:
 
     @property
     def width(self):
-        ...
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width mus be an integer")
+        elif value < 0:
+            raise ValueError("Width must be >= 0")
+        self.__width = value

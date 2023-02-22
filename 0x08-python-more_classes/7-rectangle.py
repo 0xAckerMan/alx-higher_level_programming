93d504ff85dcb2582c638312d2e24aa30274ba11
0xAckerMan <joelkores1@gmail.com>
2023-02-22 22:53:43 +0300

    Created the width getter

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''''
+
+'''
+
+
+class Rectangle:
+    '''
+    The class implementation
+    '''
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        ...

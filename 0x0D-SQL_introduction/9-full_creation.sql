f38ba4dae7fe76498ab359d9d03aabe79d467faf
0xAckerMan <joelkores1@gmail.com>
2023-03-21 23:37:53 +0300

    Task 8: Count 89

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Creates second table. adds multiple rows
-CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO first_table(id, name, score)
 VALUES 
         (1, "John", 10),

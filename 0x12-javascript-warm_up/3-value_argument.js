629534695c7b208f6f74082a94d03b776804a6fc
0xAckerMan <joelkores1@gmail.com>
2023-04-11 21:06:50 +0300

    Task 3: Value of argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,6 +4,4 @@ if (process.argv.length <= 2) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
   console.log(process.argv[2]);
-} else {
-  console.log('Argument found');
 }

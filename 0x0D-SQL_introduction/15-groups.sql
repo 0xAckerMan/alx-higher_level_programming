2cd9ec0c1372fc5c1b81ec227b5c385d549136c8
0xAckerMan <joelkores1@gmail.com>
2023-04-15 20:25:25 +0300

    Task 15: Number by score, using count()

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- COUNTS THE NUMBER OF SAME RECORDS
 
-SELECT score,COUNT(score) AS number FROM second_table;
+SELECT score,COUNT(score) AS "number" FROM second_table GROUP BY score ORDER BY number DESC;

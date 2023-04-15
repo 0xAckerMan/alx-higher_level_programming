5e373f2c38030c1fb0f0789fdc0f6ef9ebd032a8
0xAckerMan <joelkores1@gmail.com>
2023-04-15 20:36:08 +0300

    SAy my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- Lists all content in descending order
 
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;

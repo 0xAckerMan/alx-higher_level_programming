db31789fa54d671f219603d286545a31fa304d60
0xAckerMan <joelkores1@gmail.com>
2023-04-16 10:44:35 +0300

    Task 2: Filter by argv

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,14 +7,21 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(hostname='localhost', port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host='localhost',
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE %s   ORDER BY ASC",
-                   (argv[4]))
+    command = """SELECT id, name
+    FROM states
+    WHERE name LIKE BINARY '{}'
+    ORDER BY id ASC""", format(argv[4])
+    cursor.execute(command)
     rows = cursor.fetchall()
+
     for row in rows:
-        if row[1] == argv[4]:
-            print(row)
+        print(row)
+
     cursor.close()
     db.close()

4596dbf10b97d977b33694479ddbc59c7aa7be2e
0xAckerMan <joelkores1@gmail.com>
2023-04-23 10:13:19 +0300

    Task 10: Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const factorial = (n) => {
+  if (isNaN(n)){
+    return 1;
+  }else if(n === 0){
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+};
+
+const n = parseInt(process.argv[2])
+
+console.log(factorial(n))

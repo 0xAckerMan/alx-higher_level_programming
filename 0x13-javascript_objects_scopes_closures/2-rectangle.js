fbf3206d3682e1b47c7727f8e7ababa430776d6a
0xAckerMan <joelkores1@gmail.com>
2023-04-23 16:19:31 +0300

    Task 2: Rectangle #2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Class rectangle. 2 args;
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};

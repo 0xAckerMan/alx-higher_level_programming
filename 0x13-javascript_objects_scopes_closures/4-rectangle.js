7a0205f86c26cf2d6a8c1f8f0c8db01f0058f8d6
0xAckerMan <joelkores1@gmail.com>
2023-04-23 18:09:24 +0300

    Task 4: ROTATE, DOUBLE, NORMAL

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+// class rectangle. instance rotate, double, print.
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let temp = '';
+      for (let j = 0; j < this.width; j++) {
+        temp += 'X';
+      }
+      console.log(temp);
+    }
+  };
+  rotate() {
+    const newHeight = this.width;
+    const newWidth = this.height;
+    this.height = newHeight;
+    this.width = newWidth;
+  };
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  };
+};

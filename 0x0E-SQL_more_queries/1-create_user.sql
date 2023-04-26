90d41be227052c2e0c0f244c0fb201f550b8ecb0
0xAckerMan <joelkores1@gmail.com>
2023-04-26 16:39:37 +0300

    Task 1: Creating a super user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+--creates user and gives all permisions and set password
+
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;

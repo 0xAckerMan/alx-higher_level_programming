7e3d2d4e793e224c538dca5d8ba2411d1f43b4b2
0xAckerMan <joelkores1@gmail.com>
2023-07-26 22:12:47 +0300

    0. readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// reads and prints content of a file
+// passed as an argument
+
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});

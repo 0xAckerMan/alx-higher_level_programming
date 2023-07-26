248dd9e9784a6f5392c607edca0d88abf87b88fa
0xAckerMan <joelkores1@gmail.com>
2023-07-26 22:16:31 +0300

    0. readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,19 +1,11 @@
 const fs = require('fs');
 
-function readAndPrintFile (filePath) {
-  fs.readFile(filePath, 'utf-8', (err, content) => {
-    if (err) {
-      console.error('An error occurred while reading the file:');
-      console.error(err);
-    } else {
-      console.log(content);
-    }
-  });
-}
+const file = process.argv[2];
 
-if (process.argv.length !== 3) {
-  console.error('Usage: node script.js <file_path>');
-} else {
-  const filePath = process.argv[2];
-  readAndPrintFile(filePath);
-}
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
